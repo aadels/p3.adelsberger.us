cddb312e631dd0e58f54ff558e821dccd6ce41ed
ann adelsberger <adelsbergerann@gmail.com>
2013-11-20 16:54:37 -0500

    add initial documents

---
+++ madlib.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+	<title>Madlibs!</title>
+	<link rel = "stylesheet" href="css/madlib.css" type="text/css">
+	<link rel = "stylesheet" href="css/display.css" type="text/css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+</head>
+
+<html>
+<body>
+
+
+
+</body>
+</html>
