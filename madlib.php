6b845509b79d5577ebc41e6a8bbd17d066e90b88
ann adelsberger <adelsbergerann@gmail.com>
2013-11-23 22:52:36 -0500

    Revert bae6bc9..1cb3255
    This rolls back to commit bae6bc997a3c234e7b8a1f98a753228c760fc683.

--- madlib.php
+++ madlib.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-     <!--<link rel = "stylesheet" href="css/madlib.css" type="text/css"> -->     
+     <link rel = "stylesheet" href="css/madlib.css" type="text/css">      
 </head>
 
 <body>
@@ -43,10 +43,11 @@
                         beforeSend: function() {
 		                    // Display a loading message while waiting for the ajax call to complete
 		                    $('#results').html("Loading...");
-		                 }
+                		},
                         success: function(response) {
                             $('#results').html(response);
-                        }     
+                        } 
+                        
                 };
 
                 $('form').ajaxForm(options);
