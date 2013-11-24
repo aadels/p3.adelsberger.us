5beebf5d0d834a3adb669185649c94e864ea08eb
ann adelsberger <adelsbergerann@gmail.com>
2013-11-23 21:45:25 -0500

    new p3 docs and

--- madlib.php
+++ madlib.php
@@ -4,13 +4,15 @@
 	<link rel = "stylesheet" href="css/madlib.css" type="text/css">
 	<link rel = "stylesheet" href="css/display.css" type="text/css">
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </head>
 
 <html>
-<body>
+	<body>
+		<form>
+
+		</form>
 
 
 
