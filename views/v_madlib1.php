29ee94a606782edbaebc568afa4adfba97d3d048
ann adelsberger <adelsbergerann@gmail.com>
2013-12-02 06:58:25 -0500

    edit madlib 1

--- views/v_madlib1.php
+++ views/v_madlib1.php
@@ -21,5 +21,9 @@
 
             
                 <div id='results'></div>
+
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script type="text/javascript" src='js/jquery.form.js'></script>
+        <script type="text/javascript" src='js/jquery.validate.js'></script>
            
 
