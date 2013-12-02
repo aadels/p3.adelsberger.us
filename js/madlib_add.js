5336034a58f231176f2796e67d28d4f2b90053af
ann adelsberger <adelsbergerann@gmail.com>
2013-12-02 07:08:17 -0500

    change path to process1

--- js/madlib_add.js
+++ js/madlib_add.js
@@ -2,7 +2,7 @@
  var options = {
                     
     type: 'post',
-    url: 'process1.php',
+    url: '/views/process1.php',
     beforeSend: function() {   
     //Display a loading message while waiting for the ajax call to complete
         $('#results').html("Loading...");
