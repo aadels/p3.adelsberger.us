084e2b29b1c9a5650d32608edc63a3c16a064f02
ann adelsberger <adelsbergerann@gmail.com>
2013-12-04 17:33:50 -0500

    Added use strict statement

--- js/madlib_add.js
+++ js/madlib_add.js
@@ -1,6 +1,7 @@
 
 // Set up the options for ajax
-
+/*global $, jQuery, alert*/
+"use strict";
 var form1 = $('madlib1.html');
 var form2 = $('madlib2.html');
 
@@ -10,11 +11,13 @@ if(form1) {
         type: 'post',
         url: 'process1.php',
         beforeSend: function() {   
-        //Display a loading message while waiting for the ajax call to complete
+            //Display a loading message while waiting for the ajax call to complete
+            "use strict";
             $('#results').html("Loading...");
         },
-        //Hide form and display results
+            //Hide form and display results
         success: function(response) {
+            "use strict";
             $(".madlib_form").hide();
             $('#results').html(response);
         }
@@ -29,11 +32,13 @@ else {
         type: 'post',
         url: 'process2.php',
         beforeSend: function() {   
-        //Display a loading message while waiting for the ajax call to complete
+            //Display a loading message while waiting for the ajax call to complete
+            "use strict";
             $('#results').html("Loading...");
         },
         //Hide form and display results
         success: function(response) {
+            "use strict";
             $(".madlib_form").hide();
             $('#results').html(response);
         }
