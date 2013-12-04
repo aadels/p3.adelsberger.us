04d88379395b44038d86da5520175a5aa7e41338
ann adelsberger <adelsbergerann@gmail.com>
2013-12-03 21:54:10 -0500

    edit js undo css edits

--- js/madlib_add.js
+++ js/madlib_add.js
@@ -1,31 +1,44 @@
 
-/************************************************************************ 
-Set up options for ajax to display results
- ************************************************************************/
-if(form == .madlib1){  
+// Set up the options for ajax
+
+var form1 = "madlib1.html";
+var form2 = "madlib2.html";
+
+if(form1) {  
     var options = {
-               
+                        
         type: 'post',
         url: 'process1.php',
-    }
+        beforeSend: function() {   
+        //Display a loading message while waiting for the ajax call to complete
+            $('#results').html("Loading...");
+        },
+        //Hide form and display results
+        success: function(response) {
+            $(".madlib_form").hide();
+            $('#results').html(response);
+        }
+    };
+
+    $('form').ajaxForm(options);
 }
+
 else{
     var options = {
+                        
         type: 'post',
-        url: 'process1.php',
-    }
+        url: 'process2.php',
+        beforeSend: function() {   
+        //Display a loading message while waiting for the ajax call to complete
+            $('#results').html("Loading...");
+        },
+        //Hide form and display results
+        success: function(response) {
+            $(".madlib_form").hide();
+            $('#results').html(response);
+        }
+    };
+
+    $('form').ajaxForm(options);
+
 }
-   
-    beforeSend: function() {   
-    //Display a loading message while waiting for the ajax call to complete
-        $('#results').html("Loading...");
-    },
-    //Hide form and display results
-    success: function(response) {
-        $("#form-fieldset").hide();
-        $('#results').html(response);
-	}
-};
-
-$('form').ajaxForm(options);
-
