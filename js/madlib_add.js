f8db6a9b3fa664fc38a05c7a6face2db3b34d72c
ann adelsberger <adelsbergerann@gmail.com>
2013-12-04 17:53:22 -0500

    Revert "Added use strict statement"
    This reverts commit 084e2b29b1c9a5650d32608edc63a3c16a064f02.

--- js/madlib_add.js
+++ js/madlib_add.js
@@ -1,7 +1,6 @@
 
 // Set up the options for ajax
-/*global $, jQuery, alert*/
-"use strict";
+
 var form1 = $('madlib1.html');
 var form2 = $('madlib2.html');
 
@@ -11,13 +10,11 @@ if(form1) {
         type: 'post',
         url: 'process1.php',
         beforeSend: function() {   
-            //Display a loading message while waiting for the ajax call to complete
-            "use strict";
+        //Display a loading message while waiting for the ajax call to complete
             $('#results').html("Loading...");
         },
-            //Hide form and display results
+        //Hide form and display results
         success: function(response) {
-            "use strict";
             $(".madlib_form").hide();
             $('#results').html(response);
         }
@@ -32,13 +29,11 @@ else {
         type: 'post',
         url: 'process2.php',
         beforeSend: function() {   
-            //Display a loading message while waiting for the ajax call to complete
-            "use strict";
+        //Display a loading message while waiting for the ajax call to complete
             $('#results').html("Loading...");
         },
         //Hide form and display results
         success: function(response) {
-            "use strict";
             $(".madlib_form").hide();
             $('#results').html(response);
         }
