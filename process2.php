c0056ba2dc8002da5a7effdccd0cd1e0a64fcfbe
ann adelsberger <adelsbergerann@gmail.com>
2013-12-04 18:58:26 -0500

    edit form css

--- process2.php
+++ process2.php
@@ -1,11 +1,11 @@
 <?php
-echo "Ah, look at all the ".($_POST['input1'])." ".($_POST['input2'])."!
-Ah, look at all the ". ($_POST['input1'])." ".($_POST['input2'])." ! "
+echo "Ah, look at all the ".($_POST['input1'])." ".($_POST['input2'])."! <br>
+Ah, look at all the ". ($_POST['input1'])." ".($_POST['input2'])."!  <br><br>
 
-.ucfirst($_POST['input3'])." ".ucfirst($_POST['input4'])." picks up the ". ($_POST['input5'])." in a ". ($_POST['input6'])." where a ". ($_POST['input7'])." has been ".
-($_POST['input8'])." in a ".($_POST['input9']).". "
+".ucfirst($_POST['input3'])." ".ucfirst($_POST['input4'])." picks up the ". ($_POST['input5'])." in a ". ($_POST['input6'])." where a ". ($_POST['input7'])." has been.".
+($_POST['input8'])." in a ".($_POST['input9']).". <br>
 
-. ($_POST['input10'])." at the ". ($_POST['input11']).", wearing the ". ($_POST['input12'])." that she keeps in a ". ($_POST['input13'])."  by the ". ($_POST['input14']).". Who is it for?
+".ucfirst($_POST['input10'])." at the ". ($_POST['input11']).", wearing the ". ($_POST['input12'])." that she keeps in a ". ($_POST['input13'])."  by the ". ($_POST['input14']).". Who is it for?<br><br>
 
 All the ". ($_POST['input1'])." ".($_POST['input2'])." where do they all come from?
 All the ". ($_POST['input1'])." ".($_POST['input2'])." where do they all belong?";
