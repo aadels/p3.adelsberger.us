b9331aa7d58620b3bd42eae3ae96ed92551e5fe7
ann adelsberger <adelsbergerann@gmail.com>
2013-12-04 19:21:00 -0500

    edit css

--- process2.php
+++ process2.php
@@ -2,8 +2,8 @@
 echo "Ah, look at all the ".($_POST['input1'])." ".($_POST['input2'])."! <br>
 Ah, look at all the ". ($_POST['input1'])." ".($_POST['input2'])."!  <br><br>
 
-".ucfirst($_POST['input3'])." ".ucfirst($_POST['input4'])." picks up the ". ($_POST['input5'])." in a ". ($_POST['input6'])." where a ". ($_POST['input7'])." has been.".
-($_POST['input8'])." in a ".($_POST['input9']).". <br>
+".ucfirst($_POST['input3'])." ".ucfirst($_POST['input4'])." picks up the ". ($_POST['input5'])." in a ". ($_POST['input6'])." where a ". ($_POST['input7'])." has been. <br>
+".ucfirst($_POST['input8'])." in a ".($_POST['input9'])." <br>
 
 ".ucfirst($_POST['input10'])." at the ". ($_POST['input11']).", wearing the ". ($_POST['input12'])." that she keeps in a ". ($_POST['input13'])."  by the ". ($_POST['input14']).". Who is it for?<br><br>
 
