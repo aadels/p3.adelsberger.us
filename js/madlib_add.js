3788d67b197239aec33982239f7a5916d9ccec4e
ann adelsberger <adelsbergerann@gmail.com>
2013-12-05 08:41:15 -0500

    edit js

--- js/madlib_add.js
+++ js/madlib_add.js
@@ -1,5 +1,5 @@
 
-// Set up the options for ajax
+/* Set up the options for ajax
 
 var form1 = '#madlib1';
 //var form2 = '#madlib2';
@@ -42,5 +42,32 @@ else {
 
     $('form').ajaxForm(options);
 
-}
+}*/
 
+var form1 = "madlib1.html";
+var form2 = "madlib2.html";
+
+if(form1) {  
+    var options = {
+        type: 'post',
+        url: 'process1.php',
+    }
+}
+else {
+    var options = {
+        type: 'post',
+        url: 'process2.php',
+    }
+}
+   
+    beforeSend: function() {   
+    //Display a loading message while waiting for the ajax call to complete
+        $('#results').html("Loading...");
+    },
+    //Hide form and display results
+    success: function(response) {
+        $("#form-fieldset").hide();
+        $('#results').html(response);
+    }
+};
+$('form').ajaxForm(options);
