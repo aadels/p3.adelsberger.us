b7a094b6a1321ebe6be20936ffe7a58d44e059a7
ann adelsberger <adelsbergerann@gmail.com>
2013-12-05 14:55:26 -0500

    edit js

--- js/madlib_add.js
+++ js/madlib_add.js
@@ -1,15 +1,15 @@
 
 // Set up the options for ajax
 
-var form1;
+
 //var form2 = '#madlib2';
 
 //#madlib1 is the id of one of the two forms
-if(form1 =='#madlib1') {  
+if($('form') =='#madlib1') {  
     var options = {
                         
         type: 'post',
-        url: 'process1.php',
+        url:  'process1.php',
         beforeSend: function() {   
         //Display a loading message while waiting for the ajax call to complete
             $('#results').html("Loading...");
@@ -28,7 +28,7 @@ else {
     var options = {
                         
         type: 'post',
-        url: 'process2.php',
+        url:  'process2.php',
         beforeSend: function() {   
         //Display a loading message while waiting for the ajax call to complete
             $('#results').html("Loading...");
