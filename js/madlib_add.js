667fcd4faf3438db3da347361197526708b4cc16
ann adelsberger <adelsbergerann@gmail.com>
2013-12-05 19:31:45 -0500

    Revert "edit js to avoid repeating"
    This reverts commit 88c81148d598ffb3c1ff3afed1759fdc13368fda.

--- js/madlib_add.js
+++ js/madlib_add.js
@@ -1,22 +1,49 @@
-if($('form').attr('id') == 'madlib1') {
-   process_url = "process1.php";
-}
-else {
-  process_url = "process2.php";
-}
-var options = {
+
+// Set up the options for ajax
+
+
+//var value = $('#madlib1').html();
+var form_id  = $('form').attr('id');
+ console.log(form_id);
+//#madlib1 is the id of one of the two forms
+if(form_id == 'madlib1') {  
+    console.log("enter madlib1");
+    var options = {
                         
         type: 'post',
-        url: process_url,
+        url: 'process1.php',
         beforeSend: function() {   
         //Display a loading message while waiting for the ajax call to complete
             $('#results').html("Loading...");
         },
         //Hide form and display results
         success: function(response) {
+            
             $(".madlib_form").hide();
-            $('#results').html(response);
+            $('#results1').html(response);
         }
     };
 
     $('form').ajaxForm(options);
+}
+
+else {
+    console.log("entered madlib2");
+    var options = {
+                        
+        type: 'post',
+        url: 'process2.php',
+        /*beforeSend: function() {   
+        //Display a loading message while waiting for the ajax call to complete
+            $('#results').html("Loading...");
+        },*/
+        //Hide form and display results
+        success: function(response) {
+            $(".madlib_form").hide();
+            $('#results2').html(response);
+        }
+    };
+
+    $('form').ajaxForm(options);
+
+}
